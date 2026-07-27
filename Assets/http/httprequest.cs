adc967b80b527cacff6c27b48a459f125b5846b2
nigatake <phyllostachys.reticulata@gmail.com>
2026-07-27 11:49:59 +0900

    Update httprequest.cs

--- Assets/http/httprequest.cs
+++ Assets/http/httprequest.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 
 public class JsonPostExample : MonoBehaviour
 {
+
     // 送信するJSONデータの構造体
     [System.Serializable]
     public class PostData
@@ -12,12 +13,32 @@ public class JsonPostExample : MonoBehaviour
         public string model;
         public string prompt;
         public bool stream;
+        public list<int> context;
     }
 
+    [System.Serializable]
+    public class GetData
+    {
+        public string model;
+        public string created_at;
+        public string response;
+        public bool done;
+        public string done_reason;
+        public list<int> context;
+        public int total_duration;
+        public int load_duration;
+        public int prompt_eval_count;
+        public int prompt_eval_duration;
+        public int eval_count;
+        public int eval_duration;
+    }
+
+    private GetData getdata;
+        
     void Start()
     {
         // データの準備
-        PostData data = new PostData {model="LFM2.5-1.2B-JP", prompt="はろーわーるど", stream=False};
+        PostData data = new PostData {model="LFM2.5-1.2B-JP", prompt="はろーわーるど", stream=False ,context =  new List<int>()};
 
         // コルーチンの開始
         StartCoroutine(PostJsonCoroutine("http://192.168.154.105:11434/api/generate", data));
@@ -42,7 +63,9 @@ public class JsonPostExample : MonoBehaviour
             // 4. 結果の処理
             if (request.result == UnityWebRequest.Result.Success)
             {
-                Debug.Log("送信成功!: " + request.downloadHandler.text);
+                getdata = JsonUtility.FromJson<GetData>(request.downloadHandler.text);
+                Debug.Log(data);
+                return data;
             }
             else
             {
