e2c761ead360347eff88a6dded18123050b4198f
nigatake <phyllostachys.reticulata@gmail.com>
2026-09-02 17:49:23 +0900

    Update PoliceNPCMnager2.cs

--- Assets/cords/PoliceNPCMnager2.cs
+++ Assets/cords/PoliceNPCMnager2.cs
@@ -1,1 +1,22 @@
 //試作中...
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceNPCMnager2 : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public Transform Player;
+    public float speed = 100;
+    private float rotateSpeed = 100;
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        m_Rigidbody.Add
+    }
+}
